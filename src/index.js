db42c3782b392a3f97dac524405934b9e95e19af
pieter <pdut8901@gmail.com>
2020-03-19 11:55:51 +0200

    edit spacing

--- src/index.js
+++ src/index.js
@@ -35,7 +35,6 @@ export default function initTableContext(getData = () => Promise.resolve([])) {
       filters: {},
       selected: [],
       getCacheKey: state => hash(state),
-      autoApplyFilters: false
     };
 
     componentDidMount() {
